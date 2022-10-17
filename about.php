2eb5eefe749ed27c3f73e6a6eec72d80725b97e9
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2022-10-17 12:01:12 +0100

    Creating header and footer
    -header created
    -footer created
    -added some text to main page

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+    <head>
+        <title>Survey Tool</title>
+        <meta charset="utf-8">
+        <meta name="keywords" content="HTML, CSS, JavaScript, Survey, Survey tool, Questionaire">
+        <meta name="description" content="This is a web based survey tool">
+        <meta name="author" content="Umar Rajput">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="icon" href="" type="image/gif" sizes="16x16">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!-- Header Section -->
+        <?php include_once "header.php" ?>
+        
+        <!-- Main Content -->
+        <div>
+            <div>
+                <h2>The simplest and fastest way to make surveys</h2>
+                <p>Create professional looking surveys with our free to use tool, with real-time data analysis, and many more features to make your experience as quick and easy as possible.</p>
+            </div>
+            <div>
+                <img src=""/>
+            </div>
+        </div>
+        
+
+        <!-- Footer Section -->
+        <?php include_once "footer.php" ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <?php include_once "headTags.php" ?>
+    </head>
+    <body>
+        <!-- Header Section -->
+        <?php include_once "header.php" ?>
+        
+        <!-- Main Content -->
+        <div>
+            <div>
+                <h2>The simplest and fastest way to make surveys</h2>
+                <p>Create professional looking surveys with our free to use tool, with real-time data analysis, and many more features to make your experience as quick and easy as possible.</p>
+            </div>
+            <div>
+                <img src=""/>
+            </div>
+        </div>
+        
+
+        <!-- Footer Section -->
+        <?php include_once "footer.php" ?>
+    </body>
+</html>
