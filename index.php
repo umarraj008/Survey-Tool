80cb1a982c86d9e05b8af41dc237f6a45d717984
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2023-04-02 20:17:25 +0100

    Copy Link + Enter Code
    - users can now enter a code in the homepage and be redirected to survey
    - users can now copy survey link from dashboard

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en-UK">
     <head>
         <?php include_once("./includes/headTags.php"); ?>
+        <link rel="stylesheet" href="./css/index.css">
     </head>
     <body>
         <!-- Header Section -->
@@ -9,14 +10,30 @@
         
         <!-- Main Content -->
         <main>
-            <div>
-                <h1>The simplest and fastest way to make surveys</h1>
-                <h2>Create professional looking surveys with our free to use tool, with real-time data analysis, and many more features to make your experience as quick and easy as possible.</h2>
-                <a href="#">Get Started - It's Free!</a>
-                <a href="#">More Information About Us</a>
+            <div id="main-container">
+                <div id="left-container">
+                    <h1>The simplest and fastest way to make surveys</h1>
+                    <h2>Create professional looking surveys with our free to use tool, with real-time data analysis, and many more features to make your experience as quick and easy as possible.</h2>
+                    <div id="button-container">
+                        <a href="#">Get Started - It's Free!</a>
+                        <a href="#">More Information About Us</a>
+                    </div>
+                </div>
+                <div id="right-container">
+                    <img src="./resources/images/placeholder.png" width="100px" height="100px" />
+                </div>
             </div>
-            <div>
-                <img src="./resources/images/placeholder.png" width="100px" height="100px" />
+
+            <div id="survey-code-container">
+                <form action="survey.php" method="GET">
+                    <h3>Got a survey code?</h3>
+                    <p>Enter here:</p>
+                    <div id="input-container">
+                        <input type="text" name="id" placeholder="Code goes here...">
+                        <input type="submit" value="Go">
+                    </div>
+                </form>
+
             </div>
         </main>
             
