420a0f32ec22ee5114de7130fb8156c121199e8f
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2023-04-03 15:14:48 +0100

    Profile Page
    - created and styled profile page
    - profile page shows users details
    - profile page features logout function and other options that are non function as of yet.

--- profile.php
+++ profile.php
@@ -2,6 +2,7 @@
 <html lang="en-UK">
     <head>
         <?php include_once("./includes/headTags.php"); ?>
+        <link rel="stylesheet" href="./css/profile.css">
     </head>
     <body>
         <!-- Header Section -->
@@ -9,14 +10,36 @@
         
         <!-- Main Content -->
         <main>
-            <h2>Profile</h2>
+            <h1>My Account</h1>
+            
+            <div id="main-container">
+                <div class="section-container">
+                    <h2>My details</h2>
+                    <hr>
+                    <div id="input-container">
+                        <label>First Name</label>
+                        <input readonly type="text" value="<?php echo($_SESSION['user']->first_name); ?>" />
+                        <label>Last Name</label>
+                        <input readonly type="text" value="<?php echo($_SESSION['user']->last_name); ?>" />
+                        <label>Date of Birth</label>
+                        <input readonly type="text" value="<?php echo($_SESSION['user']->date_of_birth); ?>" />
+                        <label>Email</label>    
+                        <input readonly type="text" value="<?php echo($_SESSION['user']->email); ?>" />    
+                    </div>
+                </div>
+                <div class="section2-container">
+                    <h2>Settings</h2>
+                    <hr>
+                    <div id="input-container">
+                        <input type="button" value="Logout" onclick="location.href='./php/logout.php'" />
+                        <input type="button" value="Reset Password" />
+                        <input type="button" value="Enable MFA" />
+                        <input type="button" value="Delete All Surveys" />
+                        <input type="button" value="Delete Account" />
+                    </div>
+                </div>
+            </div>
 
-            <?php 
-                echo "<p>First Name: {$_SESSION["user"]->first_name} </p>";
-                echo "<p>Last Name: {$_SESSION["user"]->last_name} </p>";
-                echo "<p>Date of Birth: {$_SESSION["user"]->date_of_birth} </p>";
-                echo "<p>Email: {$_SESSION["user"]->email} </p>";
-            ?>
         </main>
             
         <!-- Footer Section -->
